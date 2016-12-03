d66c8bb568fd80ad9cd605fc23da48102d26da3e
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2016-12-03 21:55:23 +0900

    [WIP] Base controllers settings.

--- DraggableImageForm/ViewController.swift
+++ DraggableImageForm/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //TODO: 各種パーツのOutlet接続
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
