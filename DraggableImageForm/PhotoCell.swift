26c16b09917317cad0c55dbbbf838f9cf93d55af
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2016-12-04 14:58:55 +0900

    [WIP] Display images with PHAsset.

---
+++ DraggableImageForm/PhotoCell.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoCell.swift
+//  DraggableImageForm
+//
+//  Created by 酒井文也 on 2016/12/04.
+//  Copyright © 2016年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+
+    //UIパーツの配置
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoWrapView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
