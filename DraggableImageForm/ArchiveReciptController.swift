4e5d3eeaaa37aac388b7a73e09372b65ccf689c9
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2016-12-06 21:26:24 +0900

    [WIP] Create base contents of storyboard

---
+++ DraggableImageForm/ArchiveReciptController.swift
@@ -0,0 +1,21 @@
+//
+//  ArchiveReciptController.swift
+//  DraggableImageForm
+//
+//  Created by 酒井文也 on 2016/12/06.
+//  Copyright © 2016年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class ArchiveReciptController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
---
+++ DraggableImageForm/MakeReciptController.swift
@@ -0,0 +1,21 @@
+//
+//  MakeReciptController.swift
+//  DraggableImageForm
+//
+//  Created by 酒井文也 on 2016/12/06.
+//  Copyright © 2016年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class MakeReciptController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
