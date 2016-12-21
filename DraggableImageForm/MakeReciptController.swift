c395a1cb15c154f5486c459dd9b5757ed430ad19
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2016-12-22 02:43:07 +0900

    [WIP] MakeReciptController settings is doing.

--- DraggableImageForm/MakeReciptController.swift
+++ DraggableImageForm/MakeReciptController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class MakeReciptController: UIViewController {
+struct RecipeSetting {
+    static let recipeMaxCount = 20
+}
+
+class MakeReciptController: UIViewController, UINavigationControllerDelegate {
 
     //ドラッグ可能なイメージビュー
     var draggableImageView: UIImageView!
@@ -22,7 +26,7 @@ class MakeReciptController: UIViewController {
     //APIから取得したデータを格納する配列
     var apiDataList: Array<Any>!
     
-    //各種パーツのOutlet接続
+    //MakeReciptControllerクラス内のUIパーツ
     @IBOutlet weak var calendarScrollView: UIScrollView!
     @IBOutlet weak var receiptCollectionView: UICollectionView!
     @IBOutlet weak var dragAreaButton: UIButton!
@@ -30,10 +34,68 @@ class MakeReciptController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        //TODO: NavigationController関連の設定
+    }
+    
+    /* (Instance Methods) */
+
+    //セルを長押しした際の処理 ※UILongPressGestureRecognizerで実行された
+    func longPressCell(sender: UILongPressGestureRecognizer)  {
+        //TODO: UILongPressGestureRecognizer発動時に行われる処理を記載
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    /* (UICollectionViewDataSource) */
+    
+    //セクションのアイテム数を設定する
+    internal func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return RecipeSetting.recipeMaxCount
+    }
+    
+    //セルに表示する値を設定する
+    internal func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+
+        //TODO: セル関連の設定
+        
+        return cell
+    }
+    
+    //セルが選択された際の処理を設定する
+    internal func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        //TODO: セルタップ時の設定
+    }
+    
+    /* (UICollectionViewDelegateFlowLayout) */
+
+    //セル名「RecipeCell」のサイズを返す
+    internal func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        //セルのサイズを返す（配置したUICollectionViewのセルの高さを合わせておく必要がある）
+        return RecipeCell.cellOfSize()
+    }
+    
+    //セルの垂直方向の余白(margin)を設定する
+    internal func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+
+        return 0
+    }
+    
+    //セルの水平方向の余白(margin)を設定する
+    internal func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+    }
+    
+    //セル内のアイテム間の余白(margin)調整を行う
+    internal func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
 }
---
+++ DraggableImageForm/RecipeCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeCell.swift
+//  DraggableImageForm
+//
+//  Created by 酒井文也 on 2016/12/22.
+//  Copyright © 2016年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UICollectionViewCell {
+
+    //CollectionViewCellクラス内のUIパーツ
+    @IBOutlet weak var recipeImageView: UIImageView!
+    @IBOutlet weak var recipeCategoryLabel: UILabel!
+    @IBOutlet weak var recipeNameLabel: UILabel!
+
+    //セルの幅と高さを返すクラスメソッド（配置したUICollectionViewのセルの高さを合わせておく必要がある）
+    class func cellOfSize() -> CGSize {
+        let width = UIScreen.main.bounds.width / 2
+        let height = CGFloat(240)
+        return CGSize(width: width, height: height)
+    }
+}
